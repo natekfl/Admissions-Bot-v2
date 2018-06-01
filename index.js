be3234549cb5eda202888c587ca8b4570cb2ca33
natekfl <nkutzan@gmail.com>
2018-06-01 14:37:48 -0400

    Handlers and Data

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// Main variables
 const config =  require('./config.json');
 const Discord  = require('discord.js');
 
+//Set up client
 const client = new Discord.Client();
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
+
+// Register handlers
+client.on("message", message => require('./handleMessage.js')(message));
+
 
 client.login(config.token);
