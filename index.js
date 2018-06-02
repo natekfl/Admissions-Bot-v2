88e38dee6b8d0a5a97a21a4f4053eac04b8d2079
natekfl <nkutzan@gmail.com>
2018-06-02 18:38:25 -0400

    Finished bunch of commands

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 // Main variables
 const config =  require('./config/config.json');
-const globalFuncs =  require('./lib/globalFuncs.js');
 const Discord  = require('discord.js');
+const idvariables =  require('./config/idvariables.json');
 
 //Set up client
 const client = new Discord.Client();
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
+    const globalFuncs =  require('./lib/globalFuncs.js');
+
+    // Register handlers
+    client.on("message", message => require('./lib/handlers/handleMessage.js')(message));
+
+
+    //Set up unsuspend timers
+    let suspensions = require('./data/suspensions.json');
+    let time = (new Date()).getTime();
+    for (let userId in suspensions) {
+        setTimeout(function() {
+            globalFuncs.unsuspendUser(client.guilds.get(idvariables.serverId).members.get(userId))
+        }, (suspensions[userId][0] - time));
+    }
 });
-
-// Register handlers
-client.on("message", message => require('./lib/handlers/handleMessage.js')(message));
-
-
-//Set up unsuspend timers
-
 
 
 client.login(config.token);
